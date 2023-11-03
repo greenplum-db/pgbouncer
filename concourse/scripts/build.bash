9b41616972cdab38e6427e429b719b62209442d2
Darvin Harutyunyan <12006323+DarvinHarutyunyan@users.noreply.github.com>
2023-11-03 09:36:19 +0000

    Update install_dependencies

--- concourse/scripts/build.bash
+++ concourse/scripts/build.bash
@@ -4,17 +4,33 @@ set -ex
 export HOME_DIR=$PWD
 
 function install_dependencies() {
-    sudo dnf install pandoc
     case "$TARGET_OS" in
-    ubuntu*|debian*)
-        sudo apt-get install pandoc
-        ;;
-    centos*|rhel8|oel8|rocky8)
-        sudo dnf install pandoc
-        ;;
-    *)
-        echo Unknown TARGET_OS
-        ;;
+        "")
+            echo TARGET_OS is undefined
+            ;;
+        centos*|photon*)
+            yum update
+            yum install -y pandoc
+            ;;
+        rhel8|oel8|rocky8)
+            dnf update -y
+            dnf install -y epel-release
+            dnf install -y pandoc
+            ;;
+        sles*)
+            zypper update
+            zypper install -y pandoc
+            ;;
+        ubuntu*|debian*)
+            apt update
+            apt install -y pandoc
+            library_path=$(ldconfig -p | grep libreadline.so.8)
+            readline_path=$(echo "$library_path" | awk '{print $4}')
+            ln -s $readline_path /usr/lib/libreadline.so.7
+            ;;
+        *)
+            echo Unknown system: $TARGET_OS
+            ;;
     esac
 }
 
