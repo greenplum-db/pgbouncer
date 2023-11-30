ad20cd681fd9e0396828f7ded8e79a48bcb8338e
Darvin Harutyunyan <12006323+DarvinHarutyunyan@users.noreply.github.com>
2023-11-30 11:35:08 +0000

    Create 'pg_hba.conf' and do other jobs before testing

--- test/pytest.sh
+++ test/pytest.sh
@@ -7,8 +7,8 @@
 
 # set up gpdb source env
 ### START ###
-	source /usr/local/greenplum-db-devel/greenplum_path.sh
-	source ../../gpdb_src/gpAux/gpdemo/gpdemo-env.sh
+source /usr/local/greenplum-db-devel/greenplum_path.sh
+source ../../gpdb_src/gpAux/gpdemo/gpdemo-env.sh
 
 export PGDATA=$MASTER_DATA_DIRECTORY
 
@@ -44,5 +44,90 @@ NC_PORT=6668
 ## PG_PORT=6666
 ## PG_LOG=$LOGDIR/pg.log
 
+ulimit -c unlimited
+
+# The tests require that psql can connect to the PgBouncer admin
+# console.  On platforms that have getpeereid(), this works by
+# connecting as user pgbouncer over the Unix socket.  On other
+# platforms, we have to rely on "trust" authentication, but then we
+# have to skip any tests that use authentication methods other than
+# "trust".
+case `uname` in
+	MINGW*)
+		use_unix_sockets=false
+		;;
+	*)
+		use_unix_sockets=true
+		;;
+esac
+
+# System configuration checks
+SED_ERE_OP='-E'
+case `uname` in
+Linux)
+	SED_ERE_OP='-r'
+	;;
+esac
+
+if ! $use_unix_sockets; then
+	## BOUNCER_ADMIN_HOST=/tmp
+	BOUNCER_ADMIN_HOST=127.0.0.1
+
+	cp test.ini test.ini.bak
+	## echo "unix_socket_dir = ''" >> test.ini
+	sed -i 's/^unix_socket_dir =/#&/' test.ini
+	echo 'admin_users = pgbouncer' >> test.ini
+fi
+
+# System configuration checks
+if ! grep -q "^\"${USER:=$(id -un)}\"" userlist.txt; then
+	cp userlist.txt userlist.txt.bak
+	echo "\"${USER}\" \"01234\"" >> userlist.txt
+	echo "\"longpass\" \"${long_password}\"" >> userlist.txt
+	echo "\"ldapuser1\" \"123456\"" >> userlist.txt
+fi
+
+if $use_unix_sockets; then
+	sed $SED_ERE_OP -i "/unix_socket_director/s:.*(unix_socket_director.*=).*:\\1 '/tmp':" ${PGDATA}/postgresql.conf
+fi
+
+
+cat >>${PGDATA}/postgresql.conf <<-EOF
+logging_collector = off
+log_destination = stderr
+log_connections = on
+EOF
+if $use_unix_sockets; then
+    local='local'
+else
+    local='#local'
+fi
+if $pg_supports_scram; then
+	cat >${PGDATA}/pg_hba.conf <<-EOF
+	$local  p6   all                scram-sha-256
+	host   p6   all  127.0.0.1/32  scram-sha-256
+	host   p6   all  ::1/128       scram-sha-256
+	EOF
+else
+	cat >${PGDATA}/pg_hba.conf </dev/null
+fi
+cat >>${PGDATA}/pg_hba.conf <<-EOF
+$local  p4   all                password
+host   p4   all  127.0.0.1/32  password
+host   p4   all  ::1/128       password
+$local  p5   all                md5
+host   p5   all  127.0.0.1/32  md5
+host   p5   all  ::1/128       md5
+$local  all  all                trust
+host   all  all  127.0.0.1/32  trust
+host   all  all  ::1/128       trust
+EOF
+gpstop -u
+if ! $use_unix_sockets; then
+	sed -i 's/^local/#local/' pgdata/pg_hba.conf
+fi
+
+
 # python3 -m pytest ./test_admin.py::test_help
+# python3 -m pytest ./test_auth.py
 python3 -m pytest
